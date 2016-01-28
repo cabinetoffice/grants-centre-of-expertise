69b6396a7a166e8e7a00f1a2a2f65d45060e4b49
Ivan Antipov <ivantipov@gmail.com>
2016-01-28 17:32:40 +0000

    Events styling, analytics

---
+++ wp-content/themes/cocommunity/app/theme/analytics.php
@@ -0,0 +1,19 @@
+<?php
+
+// Set custom data to Yoast Google Analytics
+function co_ga_custom_settings( $gaq_push ) {
+
+	$send = array_pop( $gaq_push );
+
+	if ( is_user_logged_in() ) {
+		$current_user = wp_get_current_user();
+
+		$gaq_push[] = sprintf( "'set', 'userId', %s", $current_user->ID );
+		$gaq_push[] = sprintf( "'set', 'dimension1', '%s'", $current_user->user_login );
+	}
+
+	$gaq_push[] = $send;
+
+	return $gaq_push;
+}
+add_filter( 'yoast-ga-push-array-universal', 'co_ga_custom_settings' );
--- wp-content/themes/cocommunity/templates/functions.php
+++ wp-content/themes/cocommunity/templates/functions.php
@@ -57,3 +57,4 @@ require_once STYLESHEETPATH . '/../app/theme/sidebars.php';
 require_once STYLESHEETPATH . '/../app/theme/forums.php';
 require_once STYLESHEETPATH . '/../app/theme/members.php';
 require_once STYLESHEETPATH . '/../app/theme/groups.php';
+require_once STYLESHEETPATH . '/../app/theme/analytics.php';
