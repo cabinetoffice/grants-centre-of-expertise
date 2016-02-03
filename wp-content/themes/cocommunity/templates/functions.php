e8d8b41736b2a3470a1fb2b8b231a8ddf46005cf
Ivan Antipov <ivantipov@gmail.com>
2016-02-03 13:38:55 +0000

    Admin menu changes

--- wp-content/themes/cocommunity/templates/functions.php
+++ wp-content/themes/cocommunity/templates/functions.php
@@ -6,26 +6,43 @@ define ( 'BP_FRIENDS_SLUG', 'contacts' );
 // Autocomplete on all usernames
 define( 'BP_MESSAGES_AUTOCOMPLETE_ALL', true );
 
-// Theme setup
-function co_theme_setup() {
-	add_theme_support( 'custom-background' );
-
-	// Language files
-	if ( file_exists( WP_LANG_DIR . '/plugins/bbpress-' . WPLANG . '.mo' ) ) {
-		load_textdomain( 'bbpress', WP_LANG_DIR . '/plugins/bbpress-' . WPLANG . '.mo' );
-	}
-
-	if ( file_exists( WP_LANG_DIR . '/plugins/buddypress-' . WPLANG . '.mo' ) ) {
-		load_textdomain( 'buddypress', WP_LANG_DIR . '/plugins/buddypress-' . WPLANG . '.mo' );
-	}
-}
-add_action( 'after_setup_theme', 'co_theme_setup' );
-
 
 // Simple logging function
 function _log( $message ) {
 	error_log( print_r( $message, true ) );
 }
+
+
+// Theme setup
+function co_theme_setup() {
+	add_theme_support( 'custom-background' );
+
+	$locale = get_locale();
+
+	// Language files
+	if ( file_exists( WP_LANG_DIR . '/plugins/bbpress-' . $locale . '.mo' ) ) {
+		load_textdomain( 'bbpress', WP_LANG_DIR . '/plugins/bbpress-' . $locale . '.mo' );
+	}
+
+	if ( file_exists( WP_LANG_DIR . '/plugins/buddypress-' . $locale . '.mo' ) ) {
+		load_textdomain( 'buddypress', WP_LANG_DIR . '/plugins/buddypress-' . $locale  . '.mo' );
+	}
+}
+add_action( 'after_setup_theme', 'co_theme_setup' );
+
+
+function co_admin_bar_remove() {
+	global $wp_admin_bar;
+
+	if ( ! current_user_can( 'administrator' ) && ! current_user_can( 'keymaster' ) ) {
+		$wp_admin_bar->remove_menu('wp-logo');
+		$wp_admin_bar->remove_menu('site-name');
+		$wp_admin_bar->remove_menu('tribe-events');
+		$wp_admin_bar->remove_menu('search');
+		$wp_admin_bar->remove_menu('my-account-settings-profile');
+	}
+}
+add_action( 'wp_before_admin_bar_render', 'co_admin_bar_remove', 99 );
 
 
 // Returns a Font Awesome icon class for a mime type defined in the file upload field
@@ -58,10 +75,10 @@ function edit_sce_comment_time( $time_in_minutes ) {
 
 // Removing parent templates
 function co_remove_page_templates( $templates ) {
-    unset( $templates['template-campaign.php'] );
-    unset( $templates['template-contact.php'] );
-    unset( $templates['template-home.php'] );
-    return $templates;
+	unset( $templates['template-campaign.php'] );
+	unset( $templates['template-contact.php'] );
+	unset( $templates['template-home.php'] );
+	return $templates;
 }
 add_filter( 'theme_page_templates', 'co_remove_page_templates' );
 
@@ -80,4 +97,3 @@ require_once STYLESHEETPATH . '/../app/theme/forums.php';
 require_once STYLESHEETPATH . '/../app/theme/buddypress.php';
 require_once STYLESHEETPATH . '/../app/theme/groups.php';
 require_once STYLESHEETPATH . '/../app/theme/analytics.php';
-
